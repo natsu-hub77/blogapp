c1f2ad10fae8d87f514cbe9fd0c98a1fca146014
らの <>
2025-05-08 08:52:32 +0900

    Turbolinksの停止・JSモジュールの変更

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -1,8 +1,9 @@
 import $ from 'jquery'
-import axios from 'axios'
-import { csrfToken } from 'rails-ujs'
-
-axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+import axios from 'modules/axios'
+import {
+  ListenInactiveHeartEvent,
+  ListenActiveHeartEvent
+} from 'modules/handle_heart'
 
 const handleHeartDisplay = (hasLiked) => {
   if (hasLiked) {
@@ -24,7 +25,8 @@ const appendNewComment = (comment) => {
     `<div class="article_comment"><p>${comment.content}</p></div>`
   )
 }
-document.addEventListener('turbolinks:load', () => {
+// document.addEventListener('turbolinks:load', () => {
+document.addEventListener('DOMContentLoaded', () => {
   const dataset = $('#article-show').data()
   const articleId = dataset.articleId
 
@@ -34,6 +36,9 @@ document.addEventListener('turbolinks:load', () => {
       comments.forEach((comment) => {
         appendNewComment(comment)
       })
+    })
+    .catch((error) => {
+      window.alert('失敗')
     })
 
     handleCommentForm()
@@ -60,31 +65,6 @@ document.addEventListener('turbolinks:load', () => {
       handleHeartDisplay(hasLiked)
     })
 
-  $('.inactive-heart').on('click', () => {
-    axios.post(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status === 'OK') {
-          $('.active-heart').removeClass('hidden')
-          $('.inactive-heart').addClass('hidden')
-        }
-      })
-      .catch((e) => {
-        window.alert('Error')
-        console.log(e)
-      })
-  })
-
-  $('.active-heart').on('click', () => {
-    axios.delete(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status === 'OK') {
-          $('.active-heart').addClass('hidden')
-          $('.inactive-heart').removeClass('hidden')
-        }
-      })
-      .catch((e) => {
-        window.alert('Error')
-        console.log(e)
-      })
-  })
+    ListenInactiveHeartEvent(articleId)
+    ListenActiveHeartEvent(articleId)
 })
